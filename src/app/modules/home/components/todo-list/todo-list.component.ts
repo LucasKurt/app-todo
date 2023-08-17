ad871e14c18b7cf6c3ca6833df7c72e6e823ad9a
LucasKurt <lucascorte@outlook.com>
2023-08-17 17:05:32 -0300

    todo list logic

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+//Interface
+import { Task } from '../../model/task';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class TodoListComponent {
 
+  public tasks: Array<Task> = [
+    {name: "Minha nova task 1", checked: true},
+    {name: "Minha nova task 2", checked: false},
+  ];
+
+  public deleteTask(event: number): void {
+    this.tasks.splice(event, 1);
+  }
+
+  public deleteAllTasks(): void {
+    const confim = window.confirm("Você deseja realmente Deletar Tudo ?");
+
+    if (confim) {
+      this.tasks = [];
+    }
+  }
 }
