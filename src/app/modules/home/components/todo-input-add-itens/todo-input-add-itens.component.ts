824f134431020d78ef71300fb796f627a93714bf
LucasKurt <lucascorte@outlook.com>
2023-08-18 15:37:09 -0300

    todo input logic

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
   templateUrl: './todo-input-add-itens.component.html',
-  styleUrls: ['./todo-input-add-itens.component.scss']
+  styleUrls: ['./todo-input-add-itens.component.scss'],
 })
 export class TodoInputAddItensComponent {
+  @Output() public emitTask = new EventEmitter();
+  public newTask: string = '';
 
+  public submitTask() {
+    this.emitTask.emit(this.newTask);
+    this.newTask = '';
+  }
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 //Interface
 import { Task } from '../../model/task';
@@ -6,24 +6,42 @@ import { Task } from '../../model/task';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
-export class TodoListComponent {
+export class TodoListComponent implements DoCheck {
+  public tasks: Array<Task> = [];
 
-  public tasks: Array<Task> = [
-    {name: "Minha nova task 1", checked: true},
-    {name: "Minha nova task 2", checked: false},
-  ];
+  ngDoCheck(): void {
+    this.tasks.sort(
+      (first, last) => Number(first.checked) - Number(last.checked)
+    );
+  }
+
+  public emittedTask(event: string): void {
+    if (event.trim()) {
+      this.tasks.push({ name: event, checked: false });
+    }
+  }
 
   public deleteTask(event: number): void {
     this.tasks.splice(event, 1);
   }
 
   public deleteAllTasks(): void {
-    const confim = window.confirm("Você deseja realmente Deletar Tudo ?");
+    const confim = window.confirm('Você deseja realmente Deletar Tudo ?');
 
     if (confim) {
       this.tasks = [];
+    }
+  }
+
+  public validateInput(event: string, index: number): void {
+    if (!event) {
+      const confirm = window.confirm("Task vazia, deseja Deletar");
+
+      if(confirm) {
+        this.deleteTask(index);
+      }
     }
   }
 }
